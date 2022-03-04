a620056dd23eb7e7638bc1c0d2f8c5dad461c725
Channel <69780324+channel104257@users.noreply.github.com>
2022-03-04 15:57:05 +0800

    Create and.vhd

---
+++ Week 1/and.vhd
@@ -0,0 +1,12 @@
+library ieee ;
+use ieee.std_logic_1164.all ;
+
+entity hw2_extra is
+ port ( 	A,B : in std_logic ;
+			Y : out std_logic ) ;
+end hw2_extra ;
+
+architecture Arch of hw2_extra is
+begin
+ Y <= A and B ; 
+end Arch ;
