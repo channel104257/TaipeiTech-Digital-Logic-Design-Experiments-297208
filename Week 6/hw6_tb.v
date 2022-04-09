eab63f54b9cc4d808c9cfe802457d34c45ca8afc
Channel <69780324+channel104257@users.noreply.github.com>
2022-04-09 20:47:25 +0800

    Add files via upload

---
+++ Week 6/hw6_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns/1ps
+
+module hw6_tb;
+
+	reg cin;
+	reg [3:0] a;
+	reg [3:0] b;
+
+	wire cout;
+	wire [3:0] s;
+	
+	hw6 addsub4Bits(cin, a, b, s, cout);
+	
+	integer i;
+	
+	initial
+		begin
+		
+		$display("Testing cin = 0, the adder mode.");
+	
+		cin = 1'b0;
+	
+		for(i = 0; i < 256; i = i + 1)
+		begin
+	
+			{b, a} = i;
+			#10;
+			
+			$display($time, "\t %b + %b = %b", a, b, s);
+	
+		end
+		
+		$display("Testing cin = 1, the subtractor mode.");
+	
+		cin = 1'b1;
+	
+		for(i = 0; i < 256; i = i + 1)
+		begin
+	
+			{b, a} = i;
+			#10;
+			
+			$display($time, "\t %b - %b = %b", a, b, s);
+	
+		end
+	
+	end	
+endmodule
